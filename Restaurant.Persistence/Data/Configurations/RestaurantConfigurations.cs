7f961e62af4dbefd3847b4cc55164c1008552f12
Youssef Elkomy <youssefelkomy500@gmail.com>
2025-08-10 22:40:28 +0300

    DbContext, DbContextInitializer along with Entities Configurations using postgres database

---
+++ Restaurant.Persistence/Data/Configurations/RestaurantConfigurations.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Restaurant.Persistence.Data.Configurations.Common;
+
+namespace Restaurant.Persistence.Data.Configurations;
+internal sealed class RestaurantConfigurations : BaseEntityConfigurations<Domain.Entities.Restaurant, int>
+{
+	public override void Configure(EntityTypeBuilder<Domain.Entities.Restaurant> builder)
+	{
+		base.Configure(builder);
+
+		builder.OwnsOne(e => e.Address);
+
+		builder.HasMany(e => e.Dishes)
+				.WithOne()
+				.HasForeignKey(e => e.RestaurantId);
+	}
+}
+
---
+++ Restaurant.Persistence/Data/RestaurantDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant.Domain.Entities;
+
+namespace Restaurant.Persistence.Data;
+
+internal class RestaurantDbContext(DbContextOptions<RestaurantDbContext> options): DbContext(options)
+{
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyInformation).Assembly);
+	}
+
+	internal DbSet<Domain.Entities.Restaurant> Restaurants { get; set; }
+	internal DbSet<Dish> Dishes { get; set; }
+}
