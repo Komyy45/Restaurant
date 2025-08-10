97a0d6346582ae110303d91183d216b02f6fcb07
Youssef Elkomy <youssefelkomy500@gmail.com>
2025-08-10 16:57:59 +0300

    dockerfile and docker-compose files

--- Restaurant.Domain/Entities/Dish.cs
+++ Restaurant.Domain/Entities/Dish.cs
@@ -6,5 +6,6 @@ public sealed class Dish : BaseEntity<int>
 	public string Name { get; set; } = default!;
 	public string Description { get; set; } = default!;
 	public decimal Price { get; set; }
+	public int RestaurantId { get; set; }
 }
 
